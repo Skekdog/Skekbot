55724066240b57ac2b13156cc9502e2ada5dc55b
Skekdog <47750815+Skekdog@users.noreply.github.com>
2025-10-19 19:24:52 +0100

    dad

--- Source/Modules/dad.ts
+++ Source/Modules/dad.ts
@@ -1,8 +1,40 @@
 import { Events, Message } from "discord.js";
 import type { ModuleInterface } from "../module-interface.ts";
 
+const IM = ["im", "i'm", "i’m"];
+const MAX_LENGTH = 50;
+
 async function onMessage(message: Message) {
-	if (message.content.toLowerCase() === "hi") await message.reply(":wave:");
+	if (message.author === message.client.user) return;
+
+	const content = message.cleanContent;
+	const lowerContent = content.toLowerCase();
+
+	let startIndex;
+
+	const words = lowerContent.split(" ");
+	for (const word of words) {
+		if (word === "i") {
+			if (words[words.indexOf(word) + 1] === "am") {
+				startIndex = lowerContent.indexOf("am") + word.length + 1;
+				break;
+			}
+		}
+
+		if (IM.includes(word)) {
+			startIndex = lowerContent.indexOf(word) + word.length + 1;
+			break;
+		}
+	}
+
+	if (!startIndex || startIndex >= content.length) return;
+
+	let name: string | undefined = content.slice(startIndex);
+	name = name.split(".")[0]?.split(",")[0]?.slice(0, MAX_LENGTH).trim();
+
+	if (!name) return;
+
+	await message.reply(`Hi ${name}, I'm Skekbot!`);
 }
 
 const module: ModuleInterface = {
