4db2f9d76698119cd07b877416edce14fb26bf3f
Gurkan <hello@gurkz.me>
2026-01-07 11:51:58 +0100

    bun

--- Source/Utility/env.ts
+++ Source/Utility/env.ts
@@ -1,6 +1,8 @@
+import Bun from "bun";
+
 export const env = {
-	BOT_TOKEN: process.env["BOT_ID"],
-	PORT: Number.parseInt(process.env["PORT"] ?? "443"),
-	WEB_PASSWORD: process.env["WEB_PASSWORD"],
-	DEV_SERVER_ID: process.env["DEV_SERVER_ID"], // note: this is a string, due to discord.js
+	BOT_TOKEN: Bun.env["BOT_ID"],
+	PORT: Number.parseInt(Bun.env["PORT"] ?? "443"),
+	WEB_PASSWORD: Bun.env["WEB_PASSWORD"],
+	DEV_SERVER_ID: Bun.env["DEV_SERVER_ID"], // note: this is a string, due to discord.js
 };
--- Source/Web/server.ts
+++ Source/Web/server.ts
@@ -1,18 +1,13 @@
-import { Hono } from "hono";
 import client from "../client.ts";
 import * as z from "zod/v4";
-import { prettyJSON } from "hono/pretty-json";
-import { serveStatic } from "@hono/node-server/serve-static";
-import { bearerAuth } from "hono/bearer-auth";
-import { serve } from "@hono/node-server";
-import { zValidator } from "@hono/zod-validator";
 import { env } from "../Utility/env.ts";
+import { Elysia, file, status } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
+import { bearer as bearerAuth } from "@elysiajs/bearer";
 
 const password = env.WEB_PASSWORD!;
 
 const PORT = env.PORT;
-
-const app = new Hono();
 
 const MessageRequest = z.object({
 	channelId: z.string(),
@@ -20,54 +15,43 @@ const MessageRequest = z.object({
 	replyToId: z.string(),
 });
 
-app.use(prettyJSON());
+const app = new Elysia()
+	.get("/", "BLANK PAGE.")
+	.use(bearerAuth())
+	.use(
+		staticPlugin({
+			assets: "./Source/Web/Page",
+			prefix: "/",
+		})
+	)
+	.get("/skekbot", file("./Source/Web/Page/skekbot.html"))
+	.post(
+		"/send-message",
+		async ({ bearer, body }) => {
+			if (!bearer) return status(401, "Unauthorized");
 
-app.get("/", (c) => {
-	return c.text("BLANK PAGE.");
-});
+			if (bearer !== password) return status(403, "Forbidden");
 
-app.use(
-	"*",
-	serveStatic({
-		root: "Source/Web/Page",
-		rewriteRequestPath: (path) => {
-			// if the path already has an extension, leave it alone
-			if (path.includes(".")) {
-				return path;
+			const channel = await client.channels.fetch(body.channelId);
+			if (!channel || !channel.isSendable()) return status(400, "Invalid channel");
+
+			const replyToMessage = body.replyToId
+				? await channel.messages.fetch(body.replyToId)
+				: undefined;
+
+			if (replyToMessage) {
+				replyToMessage.reply(body.message);
+			} else {
+				channel.send(body.message);
 			}
 
-			// if not, try serving `<path>.html`
-			return `${path}.html`;
+			return "Success!";
 		},
-	})
-);
-
-app
-	.use(bearerAuth({ token: password }))
-	.post("/send-message", zValidator("json", MessageRequest), async (c) => {
-		const messageRequest = c.req.valid("json");
-
-		const channel = await client.channels.fetch(messageRequest.channelId);
-		if (!channel || !channel.isSendable()) throw new Error("Bad channel");
-
-		const replyToMessage = messageRequest.replyToId
-			? await channel.messages.fetch(messageRequest.replyToId)
-			: undefined;
-
-		if (replyToMessage) {
-			replyToMessage.reply(messageRequest.message);
-		} else {
-			channel.send(messageRequest.message);
+		{
+			body: MessageRequest,
 		}
+	)
+	.listen(PORT);
 
-		return c.text("Success!");
-	});
+console.log(`Web interface starting on port ${app.server?.hostname}:${app.server?.port}`);
 
-const server = serve({
-	port: PORT,
-	fetch: app.fetch,
-});
-
-server.addListener("listening", () => {
-	console.log(`Web interface starting on port ${PORT}.`);
-});
